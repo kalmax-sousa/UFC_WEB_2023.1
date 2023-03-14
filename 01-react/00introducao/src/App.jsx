f337a45e350944b7f7cad1260de10d3b61a0934e
Kalmax dos Santos Sousa <kalmax.santos@enivixlabs.com.br>
2023-03-14 08:42:06 -0300

    Primeira aula - Introdução ao React

---
+++ 01-react/00introducao/src/App.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div className='primeiraAula'>
+      <HelloWorld/>
+      <HelloWorld/>
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-react/00introducao/src/components/HelloWorld.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+/* Funcional
+function HelloWord(){
+    return(
+        <div>
+            <h1>Hello Word</h1>
+            <h2>Kalmax</h2>
+        </div>
+    );
+}*/
+
+// Arrow Function
+const HelloWord = () => {
+    return(
+        <div>
+            <h1>Hello Word</h1>
+            <h2>Kalmax</h2>
+        </div>
+    );
+}
+
+/* Classe
+const HelloWord = class extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello Word</h1>
+                <h2>Kalmax</h2>
+            </div>
+        );
+    }
+}*/
+
+export default HelloWord;
